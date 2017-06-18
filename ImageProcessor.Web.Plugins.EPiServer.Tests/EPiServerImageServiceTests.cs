30e1c184ac72545d8f5a7409bd2fd53f4ce2cf92
Wessel Terpstra <hello@wesselterpstra.com>
2017-06-18 14:56:37 +0200

    Added EPiServerImageService project

---
+++ ImageProcessor.Web.Plugins.EPiServer.Tests/EPiServerImageServiceTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using EPiServer.Core;
+using EPiServer.Framework.Blobs;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+using FluentAssertions;
+using ImageProcessor.Web.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace ImageProcessor.Web.Plugins.EPiServer.Tests
+{
+    [TestFixture]
+    public class EPiServerImageServiceTests
+    {
+        [Test]
+        public void IsValidRequest_CurrentContentIsImageData_ReturnsTrue()
+        {
+            // ARRANGE
+            var imageData = new Mock<ImageData>().Object;
+
+            var pageRouteHelper = CreatePageRouteHelper(imageData);
+
+            var serviceLocator = new Mock<IServiceLocator>();
+            serviceLocator.Setup(x => x.GetInstance<IPageRouteHelper>()).Returns(pageRouteHelper);
+            ServiceLocator.SetLocator(serviceLocator.Object);
+
+            var sut = CreateEPiServerImageService();
+
+            // ACT
+            var result = sut.IsValidRequest(null);
+
+            // ASSERT
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsValidRequest_CurrentContentIsPageData_ReturnsFalse()
+        {
+            // ARRANGE
+            var imageData = new Mock<PageData>().Object;
+
+            var pageRouteHelper = CreatePageRouteHelper(imageData);
+
+            var serviceLocator = new Mock<IServiceLocator>();
+            serviceLocator.Setup(x => x.GetInstance<IPageRouteHelper>()).Returns(pageRouteHelper);
+            ServiceLocator.SetLocator(serviceLocator.Object);
+
+            var sut = CreateEPiServerImageService();
+
+            // ACT
+            var result = sut.IsValidRequest(null);
+
+            // ASSERT
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void IsValidRequest_CurrentContentIsNull_ReturnsFalse()
+        {
+            // ARRANGE
+            var pageRouteHelper = CreatePageRouteHelper<IContent>(null);
+
+            var serviceLocator = new Mock<IServiceLocator>();
+            serviceLocator.Setup(x => x.GetInstance<IPageRouteHelper>()).Returns(pageRouteHelper);
+            ServiceLocator.SetLocator(serviceLocator.Object);
+
+            var sut = CreateEPiServerImageService();
+
+            // ACT
+            var result = sut.IsValidRequest(null);
+
+            // ASSERT
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task GetImage_ValidImageData_ReturnsImageDataMemoryStream()
+        {
+            // ARRANGE
+            var test123 = "test123";
+
+            var blob = new Mock<Blob>(new Uri("/notnecessaryfortest", UriKind.Relative));
+            blob.Setup(x => x.OpenRead()).Returns(new MemoryStream(Encoding.UTF8.GetBytes(test123)));
+
+            var imageData = new Mock<ImageData>();
+            imageData.Setup(x => x.BinaryData).Returns(blob.Object);
+
+            var pageRouteHelper = CreatePageRouteHelper(imageData.Object);
+
+            var serviceLocator = new Mock<IServiceLocator>();
+            serviceLocator.Setup(x => x.GetInstance<IPageRouteHelper>()).Returns(pageRouteHelper);
+            ServiceLocator.SetLocator(serviceLocator.Object);
+
+            var sut = CreateEPiServerImageService();
+
+            // ACT
+            var result = await sut.GetImage(null);
+
+            // ASSET
+            result.Should().NotBeNullOrEmpty();
+            var resultStr = Encoding.UTF8.GetString(result);
+            resultStr.Should().NotBeNullOrEmpty().And.Be(test123);
+        }
+
+        private EPiServerImageService CreateEPiServerImageService(IImageService fallBackImageService = null, bool enableLocalFallBack = false)
+        {
+            return new EPiServerImageService(fallBackImageService, enableLocalFallBack);
+        }
+
+        private IPageRouteHelper CreatePageRouteHelper<T>(T content) where T : IContent
+        {
+            var pageRouteHelper = new Mock<IPageRouteHelper>();
+
+            pageRouteHelper.Setup(x => x.Content).Returns(content);
+            pageRouteHelper.Setup(x => x.ContentLink).Returns(content?.ContentLink);
+
+            var pageData = content as PageData;
+            pageRouteHelper.Setup(x => x.Page).Returns(pageData);
+            pageRouteHelper.Setup(x => x.PageLink).Returns(pageData?.PageLink);
+            
+            return pageRouteHelper.Object;
+        }
+
+    }
+}
---
+++ ImageProcessor.Web.Plugins.EPiServer/EPiServerImageService.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+using ImageProcessor.Web.Services;
+
+namespace ImageProcessor.Web.Plugins.EPiServer
+{
+    /// <summary>
+    /// An ImageProcessor.Web.Services.IImageService for connecting ImageProcessor to the EPiServer ImageData blobs. 
+    /// Has support to fall back to another IImageService (by default the ImageProcessor.Web.Services.LocalFileImageService) if no ImageData blob is found.
+    /// </summary>
+    public class EPiServerImageService : IImageService
+    {
+        private readonly bool _fallBackEnabled;
+        private readonly IImageService _fallBackImageService;
+
+        /// <summary>
+        /// Constructor used for unit testing
+        /// </summary>
+        public EPiServerImageService(IImageService fallbackImageService, bool fallbackEnabled)
+        {
+            _fallBackImageService = fallbackImageService;
+            _fallBackEnabled = false;
+        }
+
+        /// <summary>
+        /// Parameterless constructor used by ImageProcessor
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        public EPiServerImageService()
+        {
+            var typeStr = Settings?["FallBackImageService"];
+
+            if (!string.IsNullOrWhiteSpace(typeStr))
+            {
+                var type = Type.GetType(typeStr, false);
+                
+                if (typeof(IImageService).IsAssignableFrom(type))
+                    _fallBackImageService = (IImageService) Activator.CreateInstance(type);
+            }
+
+            if (_fallBackImageService == null)
+                _fallBackImageService = new LocalFileImageService();
+
+            _fallBackEnabled = bool.TryParse(Settings?["EnableFallBack"], out bool enableFallBack) && enableFallBack;
+        }
+
+        /// <summary>
+        /// Checks with the IPageRouteHelper whether the current request resolves to a ImageData object. 
+        /// When fallBackEnabled is true it will check with the fallbackImageService if it can resolve it.
+        /// </summary>
+        public bool IsValidRequest(string path)
+        {
+            var pageRouteHelper = ServiceLocator.Current.GetInstance<IPageRouteHelper>();
+            var imageData = pageRouteHelper.Content as ImageData;
+
+            if (imageData != null)
+                return true;
+
+            if (!_fallBackEnabled)
+                return false;
+
+            try
+            {
+                if (_fallBackImageService.IsFileLocalService)
+                    path = HttpContext.Current.Server.MapPath(path);
+
+                return _fallBackImageService.IsValidRequest(path);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Will resolve the image bytes from the ImageData blobs. When not found it will make the fallBackImageService resolve the image bytes instead.
+        /// </summary>
+        public async Task<byte[]> GetImage(object id)
+        {
+            var pageRouteHelper = ServiceLocator.Current.GetInstance<IPageRouteHelper>();
+
+            var imageData = pageRouteHelper.Content as ImageData;
+
+            if (imageData == null)
+            {
+                if (_fallBackImageService.IsFileLocalService)
+                    id = HttpContext.Current.Server.MapPath(id.ToString());
+
+                return await _fallBackImageService.GetImage(id).ConfigureAwait(false);
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                var imageStream = imageData.BinaryData.OpenRead();
+                await imageStream.CopyToAsync(ms).ConfigureAwait(false);
+                return ms.ToArray();
+            }
+        }
+
+        [ExcludeFromCodeCoverage]
+        public string Prefix { get; set; } = string.Empty;
+
+        [ExcludeFromCodeCoverage]
+        public bool IsFileLocalService => false;
+
+        [ExcludeFromCodeCoverage]
+        public Dictionary<string, string> Settings { get; set; }
+
+        [ExcludeFromCodeCoverage]
+        public Uri[] WhiteList { get; set; }
+    }
+}
